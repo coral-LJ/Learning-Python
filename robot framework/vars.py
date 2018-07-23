66821f67da2347dcc3bb73839662349f6ba0c472
coral-LJ <40420805+coral-LJ@users.noreply.github.com>
2018-07-23 16:39:32 +0800

    Create vars.py
    API parameter

---
+++ robot framework/vars.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+
+
+INSIGHT_VALUE={
+        "incidentEntity.do":{"occurred": {"dateUnit": "All"}, "received": {"dateUnit": "All"}, "disposed": {"dateUnit": "All"}},
+        "findGISData":{"longitudeMin":-68.4165160546875,"latitudeMin":-16.65830847607336,"longitudeMax":-67.9441039453125,"latitudeMax":-16.31099176681346,"condition":[],"shortest":0.034332275390625,"wkt":"","type":1,"isHour":"false","beginTime":"2017-01-01","endTime":"2017-12-31"},
+        "prelist":{"todayTime":"2018-09-20","type":["ip", "if", "im", "it", "rh", "rv", "rm"],"userChoice":{"dateUnit": "MONTH", "num": 1}},
+        "incHours":{"occurredStartTime": "2017-08-09", "occurredEndTime": "2017-08-11"}
+}
+
+
+INSIGHT_HEADER={"header":{'Content-Type': 'application/json', 'token': '4A55FA95-E6AD-4A9D-8236-5A110A0D6C16'}}
+
