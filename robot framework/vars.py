7170e6410eaf216f536f7ec6e6ddf4902d8cca25
coral-LJ <40420805+coral-LJ@users.noreply.github.com>
2018-07-23 16:49:40 +0800

    Update vars.py

--- robot framework/vars.py
+++ robot framework/vars.py
@@ -4,11 +4,11 @@
 
 INSIGHT_VALUE={
         "incidentEntity.do":{"occurred": {"dateUnit": "All"}, "received": {"dateUnit": "All"}, "disposed": {"dateUnit": "All"}},
-        "findGISData":{"longitudeMin":-68.4165160546875,"latitudeMin":-16.65830847607336,"longitudeMax":-67.9441039453125,"latitudeMax":-16.31099176681346,"condition":[],"shortest":0.034332275390625,"wkt":"","type":1,"isHour":"false","beginTime":"2017-01-01","endTime":"2017-12-31"},
-        "prelist":{"todayTime":"2018-09-20","type":["ip", "if", "im", "it", "rh", "rv", "rm"],"userChoice":{"dateUnit": "MONTH", "num": 1}},
-        "incHours":{"occurredStartTime": "2017-08-09", "occurredEndTime": "2017-08-11"}
+        "findGISData":{"longitudeMin":-00.4165160546875,"latitudeMin":-00.65830847607336,"longitudeMax":-00.9441039453125,"latitudeMax":-00.31099176681346,"condition":[],"wkt":""},
+        "prelist":{"todayTime":"2000-09-20","type":["ip", "if", "im", "it", "rh", "rv", "rm"],"userChoice":{"dateUnit": "MONTH", "num": 1}},
+        "incHours":{"occurredStartTime": "2000-08-09", "occurredEndTime": "2000-08-11"}
 }
 
 
-INSIGHT_HEADER={"header":{'Content-Type': 'application/json', 'token': '4A55FA95-E6AD-4A9D-8236-5A110A0D6C16'}}
+INSIGHT_HEADER={"header":{'Content-Type': 'application/json', 'token': 'xxxxxxxx'}}
 
